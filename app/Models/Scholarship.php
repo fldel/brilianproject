eb4aac700a6fedd8f9c03f7e5ffa19fab30272b8
fldel <affectinef@gmail.com>
2025-09-24 10:31:50 +0700

    Update database

--- app/Models/Scholarship.php
+++ app/Models/Scholarship.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Scholarship extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'description', 'image', 'link',
+        'is_available', 'organizer_id', 'starts_at', 'ends_at'
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_scholarship');
+    }
 }
